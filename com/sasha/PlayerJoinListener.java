48c9ec13d723d1759facfacea2db7c69bff067c1
John <minecart94@gmail.com>
2017-12-03 18:14:03 -0600

    Allow same-IP joining

--- com/sasha/PlayerJoinListener.java
+++ com/sasha/PlayerJoinListener.java
@@ -9,30 +9,47 @@ import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.omg.CORBA.Environment;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static com.sasha.Main.lol;
 
 public class PlayerJoinListener extends PlayerListener {
 
-    public static ArrayList<SecurePlayer> securePlayers = new ArrayList<>();
+	public static ArrayList<SecurePlayer> securePlayers = new ArrayList<>();
 
-    public PlayerJoinListener() {
-        Bukkit.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_JOIN, this, Event.Priority.Highest, lol);
-    }
-    @Override
-    public void onPlayerJoin(final PlayerJoinEvent e) {
-        if (SecurePlayer.getSecurePlayer(e.getPlayer().getName()) != null) {
-            return;
-        }
-        Location location = new Location(Bukkit.getServer().getWorld("world"), 0, 128, 0);
-        SecurePlayer sp = new SecurePlayer(e.getPlayer());
-        securePlayers.add(sp);
-        if (sp.isNeedsToRegister()) {
-            e.getPlayer().sendMessage("\2478(\247dSasha\2475Auth\2478) \247eWelcome. Please \2476/register\247e to continue...");
-            return;
-        }
-        e.getPlayer().teleport(location);
-        e.getPlayer().sendMessage("\2478(\247dSasha\2475Auth\2478) \247eWelcome back. Please \2476/login \247eto continue...");
-    }
+	public PlayerJoinListener() {
+		Bukkit.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_JOIN, this, Event.Priority.Highest, lol);
+	}
+
+	@Override
+	public void onPlayerJoin(final PlayerJoinEvent e) {
+		SecurePlayer p = SecurePlayer.getSecurePlayer(e.getPlayer().getName());
+		if (SecurePlayer.getSecurePlayer(e.getPlayer().getName()) != null) {
+			return;
+		}
+		Location location = new Location(Bukkit.getServer().getWorld("world"), 0, 128, 0);
+		SecurePlayer sp = new SecurePlayer(e.getPlayer());
+		securePlayers.add(sp);
+		if (sp.isNeedsToRegister()) {
+			e.getPlayer().sendMessage(
+					"\2478(\247dSasha\2475Auth\2478) \247eWelcome. Please \2476/register\247e to continue...");
+			return;
+		}
+		if (e.getPlayer().getAddress().getHostString().equals(p.getIP())) {
+			p.setAuthed(true);
+			e.getPlayer().sendMessage(
+					"\2478(\247dSasha\2475Auth\2478) \247eWelcome back. Since you have logged in from the same IP address, authentification has been bypassed.");
+			try {
+				p.getPlayer().teleport(LocationManager.getLocation(p.getPlayer()));
+			} catch (IOException ex) {
+				ex.printStackTrace();
+				p.getPlayer().sendMessage("\2474Error whilst attempting teleportation to logout location!");
+			}
+			return;
+		}
+		e.getPlayer().teleport(location);
+		e.getPlayer().sendMessage(
+				"\2478(\247dSasha\2475Auth\2478) \247eWelcome back. Please \2476/login \247eto continue...");
+	}
 }
